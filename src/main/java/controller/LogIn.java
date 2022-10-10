d4d51967a7830731e0f919044528b711654162a3
simonwandera <simonwandera12@gmail.com>
2022-10-10 10:02:01 +0300

    Repair logo display

--- src/main/java/controller/LogIn.java
+++ src/main/java/controller/LogIn.java
@@ -98,7 +98,7 @@ public class LogIn extends HttpServlet {
      "     <div class=\"col-lg-4 mx-auto\">"+
       "      <div class=\"auth-form-light text-left py-5 px-4 px-sm-5\">"+
        "       <div class=\"brand-logo\">"+
-        "        <img src=\"./images/logo.svg\">"+
+        "        <img src=\"./assets/images/logo.svg\">"+
          "     </div>"+
           "    <h4>Badili Sacco! jipange uzeeni</h4>"+
            "   <h6 class=\"font-weight-light\">Sign in to continue </h6>" +
